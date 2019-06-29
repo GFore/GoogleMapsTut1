7f8b0c11686d22b65b640b14443a73a23eb7f923
Greg Foreman <gfore@mindspring.com>
2019-06-28 23:52:00 -0400

    animating trail

--- src/Map.js
+++ src/Map.js
@@ -12,10 +12,54 @@ class Map extends React.Component {
     { lat: 18.558744, lng: -68.387929 }
   ];
 
+  velocity = 5
+  initialDate = new Date()
+
+  getDistance = () => {
+    // seconds between when the component loaded and now
+    const differentInTime = (new Date() - this.initialDate) / 1000 // pass to seconds
+    return differentInTime * this.velocity // d = v*t -- thanks Newton!
+  }
+
+  componentDidMount = () => {
+    this.interval = window.setInterval(this.consoleDistance, 1000)
+  }
+
+  componentWillUnmount = () => {
+    window.clearInterval(this.interval)
+  }
+
+  consoleDistance = () => {
+    console.log("getDistance >>> ", this.getDistance())
+  }
+
+  componentWillMount = () => {
+    this.path = this.path.map((coordinates, i, array) => {
+      if (i === 0) {
+        return { ...coordinates, distance: 0 } // it begins here! 
+      }
+      const { lat: lat1, lng: lng1 } = coordinates
+      const latLong1 = new window.google.maps.LatLng(lat1, lng1)
+
+      const { lat: lat2, lng: lng2 } = array[0]
+      const latLong2 = new window.google.maps.LatLng(lat2, lng2)
+
+      // in meters:
+      const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
+        latLong1,
+        latLong2
+      )
+
+      return { ...coordinates, distance }
+    })
+
+    console.log(this.path)
+  }
+
+
   render = () => {
     return (
       <GoogleMap defaultZoom={16} defaultCenter={{ lat: 18.559008, lng: -68.388881 }} >
-        {/* <Marker position={{ lat: 18.559024, lng: -68.388886 }} /> */}
         <Polyline path={this.path} options={{ strokeColor: "#FF0000 " }} />
         <Marker position={this.path[this.path.length - 1]} />
       </GoogleMap>
